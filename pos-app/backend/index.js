71bee95d07d08476983b231f18a21f318c9c5dca
kelumsri <121974398+kelumsri@users.noreply.github.com>
2023-02-19 15:08:05 +0530

    connected backend to database

--- pos-app/backend/index.js
+++ pos-app/backend/index.js
@@ -10,8 +10,38 @@ const db = mysql.createConnection({
   database: "inventorymanagement",
 });
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json("hello this is my test backend");
+});
+
+app.get("/product", (req, res) => {
+  const q = "SELECT * FROM inventorymanagement.product";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/product", (req, res) => {
+  const q =
+    "INSERT INTO product (`id`,`name`,`quntity`,`byingprice`,`sellingprice`,`category`,`min_stock_level`,`description`) VALUES (?)";
+  const values = [
+    "id from backend",
+    "name from backend",
+    "quntity from backend",
+    "byingprice from backend",
+    "sellingprice from backend",
+    "category from backend",
+    "min_stock_level from backend",
+    "description from backend",
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
 app.listen(8801, () => {
