9745ad444d67a4ae0d9714450ac33b3c0bca5520
salamafazlul <105430673+salamafazlul@users.noreply.github.com>
2023-06-14 20:04:16 +0530

    adding email functionality to checkout

--- server/routes/Email_Invoice.js
+++ server/routes/Email_Invoice.js
@@ -7,34 +7,44 @@ const { Invoice_Product, Product, Invoice, User } = require("../models");
 router.post("/api/generatePdf", async (req, res) => {
   const { email, invoice_id } = req.body;
   try {
-    // find the invoice with the given invoice_id and its associated invoice products
-    const invoiceProduct = await Invoice_Product.findAll({
+    // Find the invoice with the given invoice_id and its associated invoice products
+    const invoiceProducts = await Invoice_Product.findAll({
       where: { invoice_id },
-      include: [
-        { model: Product, attributes: ["product_name"] },
-      ],
-    });
-    const invoice = await Invoice.findOne({
-      where: { invoice_id },
-      include: [
-        { model: User, attributes: ["user_name"] },
-      ],
+      include: [{ model: Product, attributes: ["product_name"], as: "Product" }],
     });
 
-    if (!invoiceProduct) {
+    const invoice = await Invoice.findOne({
+      where: { invoice_id },
+      include: [{ model: User, attributes: ["user_name"] }],
+    });
+
+    if (!invoiceProducts) {
       return res.status(404).send("Invoice not found");
     }
 
-    // create a new PDF document
+    // Transform the data using map
+    const transformedData = invoiceProducts.map((invoiceProduct) => {
+      const { product_name } = invoiceProduct.Product;
+      return {
+        product_id: invoiceProduct.product_id,
+        product_name,
+        quantity: invoiceProduct.quantity,
+        price: invoiceProduct.price,
+        discount: invoiceProduct.discount,
+        amount: invoiceProduct.amount,
+      };
+    });
+
+    // Create a new PDF document
     const doc = new PDFDocument();
 
-    // pipe the PDF document to a buffer
+    // Pipe the PDF document to a buffer
     const buffers = [];
     doc.on("data", buffers.push.bind(buffers));
     doc.on("end", () => {
       const pdfData = Buffer.concat(buffers);
 
-      // create a nodemailer transport
+      // Create a nodemailer transport
       const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
@@ -43,7 +53,7 @@ router.post("/api/generatePdf", async (req, res) => {
         },
       });
 
-      // create the email message
+      // Create the email message
       const message = {
         from: "salama.fhf@gmail.com",
         to: email,
@@ -63,11 +73,11 @@ router.post("/api/generatePdf", async (req, res) => {
         ],
       };
 
-      // send the email
+      // Send the email
       transporter.sendMail(message, (error, info) => {
         if (error) {
           console.log(error);
-          res.status(500).send("Failed to send invoice");
+          res.status(500).send("Failed to send the invoice");
         } else {
           console.log("Invoice sent successfully");
           res.status(200).send("Invoice sent successfully");
@@ -75,37 +85,58 @@ router.post("/api/generatePdf", async (req, res) => {
       });
     });
 
-    // add some content to the PDF document
-    doc.fontSize(25).text("BY TECH POS", { align: "center" });
-    doc.fontSize(16).text(`Invoice ${invoice_id}`, { align: "center" });
-   
+    // Add some content to the PDF document
+    doc.fillColor("blue").fontSize(25).text("BY TECH POS", { align: "center" });
+    doc.fillColor("blue").fontSize(16).text(`Invoice ${invoice_id}`, { align: "center" });
 
-    // add the invoice details to the PDF document
+    // Add the invoice details to the PDF document
     doc.moveDown();
-    doc.fontSize(12).text(`Date: ${invoice.date}`);
-    doc.fontSize(16).text(`Cashier: ${invoice.User.user_name}`);
-    doc.fontSize(16).text(`Gross Amount: ${invoice.total}`);
-    doc.fontSize(16).text(`Bill Discount: ${invoice.discount} %`);
-    doc.fontSize(16).text(`Net Amount: ${invoice.total - (invoice.total*invoice.discount/100)}`);
+    const invoiceDetailsTop = doc.y; // Store the current y-coordinate for invoice details positioning
+    doc.fillColor("black").fontSize(12).text(`Date: ${invoice.date}`);
+    doc.fillColor("black").fontSize(12).text(`Cashier: ${invoice.User.user_name}`);
+    doc.fillColor("black").fontSize(12).text(`Gross Amount: ${invoice.total}`);
+    doc.fillColor("black").fontSize(12).text(`Bill Discount: ${invoice.discount} %`);
+    doc.fillColor("black").fontSize(12).text(`Net Amount: ${invoice.total - (invoice.total * invoice.discount) / 100}`);
     doc.moveDown();
-    doc.fontSize(16).text("Products:");
 
-    invoiceProduct.forEach((invoiceProduct) => {
-      const product = invoiceProduct.Product;
-      doc.moveDown();
-      doc.fontSize(12).text(`Product name: ${product.product_name}`);
-      doc.fontSize(12).text(`Quantity: ${invoiceProduct.quantity}`);
-      doc.fontSize(12).text(`Price: ${invoiceProduct.price}`);
-      doc.fontSize(12).text(`Discount(%): ${invoiceProduct.discount}`);
-      doc.fontSize(12).text(`Amount: ${invoiceProduct.amount}`);
+    // Generate the table dynamically
+
+    const table = [["Product ID", "Name", "Quantity", "Price", "Discount", "Amount"]];
+    transformedData.forEach((element) => {
+      const { product_id, product_name, quantity, price, discount, amount } = element;
+      table.push([product_id, product_name, quantity, price, discount, amount]);
     });
-    
 
-    // end the PDF document
+    const tableTop = invoiceDetailsTop + 50; // Adjust the tableTop value to create space between invoice details and the table
+    const tableLeft = 50;
+    const cellPadding = 10;
+    const rowHeight = 30;
+    const colWidth = (doc.page.width - tableLeft * 2) / table[0].length;
+
+    doc.font("Helvetica-Bold").fontSize(12);
+    doc.fillColor("black");
+
+    for (let i = 0; i < table.length; i++) {
+      const row = table[i];
+      for (let j = 0; j < row.length; j++) {
+        const cellValue = row[j];
+        const cellTop = tableTop + (i + 1) * rowHeight;
+        const cellLeft = tableLeft + j * colWidth;
+
+        doc.rect(cellLeft, cellTop, colWidth, rowHeight).stroke();
+        doc.text(cellValue.toString(), cellLeft + cellPadding, cellTop + cellPadding, {
+          width: colWidth - cellPadding * 2,
+          align: "left",
+          valign: "top",
+        });
+      }
+    }
+
+    // Finalize the PDF and end the response
     doc.end();
   } catch (error) {
-    console.log(error);
-    res.status(500).send("Failed to generate PDF");
+    console.error(error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
